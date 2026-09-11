8abb7e133b77019c9f65ef0eb05ddaf060889b39
Lidya Langana <122570694+lidyaa55@users.noreply.github.com>
2026-09-11 11:32:39 -0700

    Pico lcd touch setup (#48)
    * Add mock LED backend integration
    
    * Added tests for the mock LED, and SetLED
    
    * Add mock LED GUI display
    
    * change the screen
    
    * Add pytest unit tests for mock LED
    
    * Move Board LED indicator to top-right of GUI
    
    * fix formatting
    
    * fix formatting by running format.bat/format.sh
    
    * Implement new heater/chiller state indicator and animation in GUI
    
    * Add Pico LCD + touch README and test files
    
    * format

---
+++ demos/demo_setup.bat
@@ -0,0 +1,3 @@
+# This batch file downloads the ili9341.py and uFT6336U.py files from their respective GitHub repositories and saves them in the current directory.
+curl -L -o ili9341.py https://raw.githubusercontent.com/rdagger/micropython-ili9341/refs/heads/master/ili9341.py
+curl -L -o uFT6336U.py https://raw.githubusercontent.com/fantasticdonkey/uFT6336U/refs/heads/master/uFT6336U.py
